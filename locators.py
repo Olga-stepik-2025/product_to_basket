b5f8761e51f8baf910a0568fa9490f26c1d2f77b
Olga <andronovaolga.2022@gmail.com>
2025-11-18 22:15:16 +0500

    Update locators.py
    Находится в папке pages в проекте PyTest

--- locators.py
+++ locators.py
@@ -14,18 +14,18 @@ class LoginPageLocators():
 
 class ProductPageLocators():
     """Селекторы для страницы товара"""
-    
+
     # Название товара
     PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
-    
+
     # Цена товара
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
-    
+
     # Кнопка "Добавить в корзину"
     ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
-    
+
     # Сообщение об добавлении в корзину (содержит название товара)
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alertinner")
-    
+
     # Сообщение со стоимостью корзины
-    BASKET_TOTAL_MESSAGE = (By.CSS_SELECTOR, ".basket-mini .basket-mini-b strong")
+    BASKET_TOTAL_MESSAGE = (By.CSS_SELECTOR, ".alert-info .alertinner")

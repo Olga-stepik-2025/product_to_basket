2b9d37ddf06f580b4771428c5a96cca87621ce35
Olga <andronovaolga.2022@gmail.com>
2025-11-18 22:28:21 +0500

    Create test_main_page.py
    В папке pages

---
+++ test_main_page.py
@@ -0,0 +1,15 @@
+from pages.main_page import MainPage
+
+def test_guest_can_go_to_login_page(browser):
+    """Тест: гость может перейти на страницу логина"""
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+def test_guest_should_see_login_link(browser):
+    """Тест: гость должен видеть ссылку логина"""
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()  # ← Вызов метода проверки
